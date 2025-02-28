33519cd760c4b9af89c87e4081fb6b56ca3bcb72
Sadman Shawmik <hello@sadmansh.com>
2025-02-27 22:40:28 -0500

    Replace blur hero with dots

--- src/app/components/home/hero.js
+++ src/app/components/home/hero.js
@@ -2,15 +2,7 @@ import Link from 'next/link'
 
 const Hero = () => {
 	return (
-		<div className="relative overflow-hidden flex flex-col items-center justify-center h-screen p-8 bg-grey-600 [clip-path:inset(0)] rounded-b-2xl">
-			<div className="absolute top-0 left-0 z-0 flex justify-between h-full w-full blur-[200px]">
-				<div className="w-16 bg-violet-400"></div>
-				<div className="w-16 bg-sky-400"></div>
-				<div className="w-16 bg-teal-400"></div>
-				<div className="w-16 bg-lime-400"></div>
-				<div className="w-16 bg-amber-300"></div>
-				<div className="w-16 bg-red-300"></div>
-			</div>
+		<div className="homepage-hero relative overflow-hidden flex flex-col items-center justify-center h-screen p-8 bg-grey-600 rounded-b-2xl">
 			<div className="relative z-1 max-w-4xl mx-auto text-center">
 				<h1 className="text-5xl md:text-6xl lg:text-7xl font-bold font-[family-name:var(--font-poly-sans)] leading-5xl">
 					Web design and development for small businesses
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -10,15 +10,7 @@ export const metadata = {
 const Contact = () => {
 	return (
 		<main className="">
-			<div className="relative overflow-hidden flex flex-col items-center justify-center h-[60vh] p-8 bg-grey-600 [clip-path:inset(0)] rounded-b-2xl">
-				<div className="absolute top-0 left-0 z-0 flex justify-between h-full w-full blur-[200px]">
-					<div className="w-32 bg-indigo-600"></div>
-					<div className="w-32 bg-orange-600"></div>
-					<div className="w-32 bg-green-600"></div>
-					<div className="w-32 bg-blue-600"></div>
-					<div className="w-32 bg-yellow-600"></div>
-					<div className="w-32 bg-pink-600"></div>
-				</div>
+			<div className="contact-hero relative overflow-hidden flex flex-col items-center justify-center h-[60vh] p-8 bg-grey-600 rounded-b-2xl">
 				<div className="relative max-w-4xl mx-auto text-center">
 					<h2 className="text-4xl font-bold font-[family-name:var(--font-poly-sans)]">
 						Let's work together.
